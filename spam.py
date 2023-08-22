0289dd0bd4eabc89cfccc3f2077d5a1344926367
rx923 <rx923@proton.me>
2023-08-22 06:58:04 +0300

    Initial commit

---
+++ spam.py
@@ -0,0 +1,25 @@
+def spam1():
+
+    def spam2():
+
+        def spam3():
+            z = ' even'
+            z += y
+            print("In spam3, locals are {}".format(locals()))
+            return z
+
+        y = ' more ' + x
+        y += spam3()
+        print('In spam2, locals are {}'.format(locals()))
+        return y
+
+    x = 'spam'  # x must exist before spam2 is called
+    x += spam2()    # do not combine these assignments
+    print('In spam1, locals are {}'.format(locals()))
+    return x
+
+
+print(spam1())
+print(locals())
+print(globals())
+
